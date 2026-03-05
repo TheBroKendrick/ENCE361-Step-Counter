bfe20197aacf52ad73d6a9f243d558ecde17fd7a
tmc202 <tmc202@uclive.ac.nz>
2026-03-06 12:08:35 +1300

    Created task_button module

---
+++ Core/Inc/task_buttons.h
@@ -0,0 +1,14 @@
+/*
+ * task_buttons.h
+ *
+ *  Created on: 6/03/2026
+ *      Author: Liam du Plessis - ldu60
+ */
+
+#ifndef INC_TASK_BUTTONS_H_
+#define INC_TASK_BUTTONS_H_
+
+
+void button_task_execute(void);
+
+#endif /* INC_TASK_BUTTONS_H_ */
---
+++ Core/Src/task_buttons.c
@@ -0,0 +1,35 @@
+/*
+ * task_buttons.c
+ *
+ *  Created on: 6/03/2026
+ *      Author: Liam du Plessis - ldu60
+ */
+
+#include "rgb.h"
+#include "buttons.h"
+
+
+void button_task_execute(void)
+{
+	  if (buttons_checkButton(UP) == PUSHED)
+	  {
+		  rgb_led_toggle(RGB_UP);
+	  }
+
+	  if (buttons_checkButton(DOWN) == PUSHED)
+	  {
+		  rgb_led_toggle(RGB_DOWN);
+	  }
+
+	  if (buttons_checkButton(RIGHT) == PUSHED)
+	  {
+		  rgb_led_toggle(RGB_RIGHT);
+	  }
+
+	  if (buttons_checkButton(LEFT) == PUSHED)
+	  {
+		  rgb_led_toggle(RGB_LEFT);
+	  }
+
+	  buttons_update();
+}
